b811b0630a532a0b4ef8230be378cce12f6aa44f
dnfbd <dnfbd@naver.com>
2023-07-24 09:38:17 +0900

    Ably making only vanilla markup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Swiper ('.img-swiper .swiper-container',{
+  loop: true,
+  autoplay: {
+    delay:5000
+  },
+  navigation: {
+    prevEl: '.img-swiper .swiper-prev',
+    nextEl: '.img-swiper .swiper-next'
+  }
+});
